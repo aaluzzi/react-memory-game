d7595b7ea6f7d1178e8f079e737774c631dee6e9
Austin Aluzzi <aaaluzzi@cpp.edu>
2023-01-12 13:47:48 -0800

    Add button to start game

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './global.css';
 import React, {useState} from 'react';
 import GameBoard from './components/GameBoard';
+import Card from  './components/Card';
 
 function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [playing, setPlaying] = useState(false);
 
   const addPoint = () => {
     setScore(score + 1);
   }
 
+  const startGame = () => {
+    setPlaying(true);
+  }
+
   const loseGame = () => {
+    setPlaying(false);
     if (score > highScore) {
       setHighScore(score);
     }
@@ -20,7 +27,10 @@ function App() {
   return (
     <div className="app">
       <div className="score">Score: <b>{score}</b> - Best: <b>{highScore}</b></div>
-      <GameBoard addPoint={addPoint} loseGame={loseGame} />
+      {playing 
+        ? <GameBoard addPoint={addPoint} loseGame={loseGame} />
+        : <Card onClick={startGame} emoji="▶️"></Card>
+      }
     </div>
   );
 }
